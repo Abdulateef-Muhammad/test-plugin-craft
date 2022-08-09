cb51f5baefe3cf4fa6cbc4aecee53f37c22519fb
Abdulateef <abdulateef@onegr.com>
2022-08-09 15:55:44 +0300

    initial commit

---
+++ plugins/test/src/assetbundles/testcpsection/dist/js/Test.js
@@ -0,0 +1,11 @@
+/**
+ * test plugin for Craft CMS
+ *
+ * Test Field JS
+ *
+ * @author    Abdulateef
+ * @copyright Copyright (c) 2022 Abdulateef
+ * @link      github.com/Abdulateef-Muhammad
+ * @package   Test
+ * @since     1.0.0
+ */
---
+++ plugins/test/src/translations/en/test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * test plugin for Craft CMS 3.x
+ *
+ * some description
+ *
+ * @link      github.com/Abdulateef-Muhammad
+ * @copyright Copyright (c) 2022 Abdulateef
+ */
+
+/**
+ * test en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('test', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Abdulateef
+ * @package   Test
+ * @since     1.0.0
+ */
+return [
+    'test plugin loaded' => 'test plugin loaded',
+];
